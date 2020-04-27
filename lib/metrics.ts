a60418d640db2701d673d469814c323170bccad0
Andre Jasiskis <midea.andre@gmail.com>
2020-04-27 13:17:06 +0100

    feat: create metrics tracker

---
+++ lib/metrics.ts
@@ -0,0 +1,42 @@
+import * as _ from 'lodash';
+
+interface Metrics {
+  fetchCallGraphBuilder?: number;
+  getMvnClassPath?: number;
+  getGradleClassPath?: number;
+  getEntrypoints?: number;
+  generateCallGraph?: number;
+  mapClassesPerJar?: number;
+  getCallGraph?: number;
+}
+
+const metricsState: { [x in keyof Metrics]: [number, number] | undefined } = {};
+
+function start(label: keyof Metrics) {
+  metricsState[label] = process.hrtime();
+}
+
+function stop(label: keyof Metrics) {
+  const previous = metricsState[label];
+  metricsState[label] = process.hrtime(previous);
+}
+
+function getMetrics(): Metrics {
+  return _(metricsState)
+    .mapValues<number>(
+      (hrtime) => _.get(hrtime, '[0]', 0) + _.get(hrtime, '[1]', 0) / 1e9,
+    )
+    .value();
+}
+
+async function timeIt<T>(
+  label: keyof Metrics,
+  fn: () => Promise<T>,
+): Promise<T> {
+  start(label);
+  const x = await fn();
+  stop(label);
+  return x;
+}
+
+export { Metrics, timeIt, getMetrics };
---
+++ test/metrics.test.ts
@@ -0,0 +1,31 @@
+import * as metrics from '../lib/metrics';
+
+describe('timing metrics', () => {
+  test('should run function being timed', () => {
+    const result = metrics.timeIt('fetchCallGraphBuilder', () =>
+      Promise.resolve(10),
+    );
+    expect(result).resolves.toBe(10);
+  });
+
+  test('should time function correctly', async () => {
+    const mock = jest.spyOn(process, 'hrtime');
+    mock
+      .mockImplementationOnce(() => [0, 10])
+      .mockImplementationOnce(() => [10, 20]);
+
+    await metrics.timeIt('fetchCallGraphBuilder', () => Promise.resolve(10));
+    expect(mock).toBeCalledTimes(2);
+    expect(mock.mock.calls).toEqual([[], [[0, 10]]]);
+  });
+
+  test('should return metrics correctly', async () => {
+    const mock = jest.spyOn(process, 'hrtime');
+    mock
+      .mockImplementationOnce(() => [0, 10])
+      .mockImplementationOnce(() => [10, 20]);
+
+    await metrics.timeIt('fetchCallGraphBuilder', () => Promise.resolve(10));
+    expect(metrics.getMetrics()).toEqual({ fetchCallGraphBuilder: 10.00000002 });
+  });
+});
