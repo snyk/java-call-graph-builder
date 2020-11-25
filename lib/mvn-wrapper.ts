dfaf996938f9966c301eac08223128c1c836741b
Alex Muscar <alex.muscar@snyk.io>
2020-11-25 10:39:33 +0000

    feat: improve class path discovery for maven projects

--- lib/mvn-wrapper.ts
+++ lib/mvn-wrapper.ts
@@ -1,196 +1,170 @@
 import 'source-map-support/register';
-import { execute } from './sub-process';
-import { ClassPathGenerationError, EmptyClassPathError } from './errors';
-import * as fs from 'fs';
 import * as path from 'path';
+import * as fs from 'fs';
+import * as xmlJs from 'xml-js';
+import { ClassPath } from './classpath';
 import * as tmp from 'tmp';
+import { execute } from './sub-process';
+import { MalformedModulesSpecError } from './errors';
+import { timeIt } from './metrics';
 import { debug } from './debug';
 
-const os = require('os');
+// Low level helper functions
 
-async function runCommandWithOutputToFile(
-  f: (string) => Promise<void>,
+export async function withOutputToTemporaryFile(
+  f: (fileName: string) => Promise<void>,
 ): Promise<string> {
   // NOTE(alexmu): We have to do this little dance with output written to files
   // because that seems to be the only way to get the output without having to
   // parse maven logs
-  const outputName = tmp.tmpNameSync();
+  const file = tmp.fileSync({ discardDescriptor: true });
   try {
-    await f(outputName);
-    return fs.readFileSync(outputName, 'utf8');
-  } catch (error) {
-    debug(`Failed to execute command. ${error}`);
-    throw error;
-  } finally {
-    try {
-      fs.unlinkSync(outputName);
-    } catch (error) {
-      debug(`unlinkSync failed. ${error}`);
-    }
-  }
-}
-
-async function getDependenciesClassPath(targetPath: string): Promise<string> {
-  return runCommandWithOutputToFile(async (outputFile) => {
-    await execute(
-      'mvn',
-      [
-        'dependency:build-classpath',
-        `-Dmdep.outputFile=${outputFile}`,
-        '-f',
-        targetPath,
-      ],
-      { cwd: targetPath },
-    );
-  });
-}
-
-async function getOutputDirectory(targetPath: string): Promise<string> {
-  return runCommandWithOutputToFile(async (outputFile) => {
-    await execute(
-      'mvn',
-      [
-        'help:evaluate',
-        '-Dexpression="project.build.outputDirectory"',
-        `-Doutput=${outputFile}`,
-        '-f',
-        targetPath,
-      ],
-      { cwd: targetPath },
-    );
-  });
-}
-
-export function buildFullClassPath(
-  dependenciesClassPath: string,
-  outputDirectory: string,
-): string {
-  if (dependenciesClassPath.length === 0) {
-    debug('Failed to determine the dependencies classpath.');
-  }
-  if (outputDirectory.length === 0) {
-    debug('Failed to determine the project output directory.');
-  }
-  const sanitisedOutputDirectory = outputDirectory.trim();
-  let sanitisedClassPath = dependenciesClassPath.trim();
-  while (sanitisedClassPath.endsWith(path.delimiter)) {
-    sanitisedClassPath = sanitisedClassPath.slice(0, -1);
-  }
-  if (sanitisedClassPath.length === 0) {
-    return sanitisedOutputDirectory;
-  } else if (sanitisedOutputDirectory.length === 0) {
-    return sanitisedClassPath;
-  } else {
-    return `${sanitisedClassPath}${path.delimiter}${sanitisedOutputDirectory}`;
-  }
-}
-
-// NOTE(alexmu): This is deprecated, and will be removed in the future
-export function getMvnCommandArgsForMvnExec(targetPath: string): string[] {
-  return process.platform === 'win32'
-    ? [
-        '-q',
-        'exec:exec',
-        '-Dexec.classpathScope="compile"',
-        '-Dexec.executable="cmd"',
-        '-Dexec.args="/c echo %classpath"',
-        '-f',
-        targetPath,
-      ]
-    : [
-        '-q',
-        'exec:exec',
-        '-Dexec.classpathScope="compile"',
-        '-Dexec.executable="echo"',
-        '-Dexec.args="%classpath"',
-        '-f',
-        targetPath,
-      ];
-}
-
-// NOTE(alexmu): This is deprecated, and will be removed in the future
-function getMvnCommandArgsForDependencyPlugin(targetPath: string): string[] {
-  return ['dependency:build-classpath', '-f', targetPath];
-}
-
-// NOTE(alexmu): This is deprecated, and will be removed in the future
-export function parseMvnDependencyPluginCommandOutput(
-  mvnCommandOutput: string,
-): string[] {
-  const outputLines = mvnCommandOutput.split(os.EOL);
-  const mvnClassPaths: string[] = [];
-  let startIndex = 0;
-  let i = outputLines.indexOf('[INFO] Dependencies classpath:', startIndex);
-  while (i > -1) {
-    if (outputLines[i + 1] !== '') {
-      mvnClassPaths.push(outputLines[i + 1]);
-    }
-    startIndex = i + 2;
-    i = outputLines.indexOf('[INFO] Dependencies classpath:', startIndex);
-  }
-
-  return mvnClassPaths;
-}
-
-// NOTE(alexmu): This is deprecated, and will be removed in the future
-export function parseMvnExecCommandOutput(mvnCommandOutput: string): string[] {
-  return mvnCommandOutput.trim().split(os.EOL);
-}
-
-// NOTE(alexmu): This is deprecated, and will be removed in the future
-export function mergeMvnClassPaths(classPaths: string[]): string {
-  // this magic joins all items in array with :, splits result by : again
-  // makes Set (to uniq items), create Array from it and join it by : to have
-  // proper path like format
-  return Array.from(new Set(classPaths.join(':').split(':'))).join(':');
-}
-
-// NOTE(alexmu): This is deprecated, and will be removed in the future
-async function getClassPathFromMvnLegacy(targetPath: string): Promise<string> {
-  let classPaths: string[] = [];
-  let args: string[] = [];
-  try {
-    try {
-      // there are two ways of getting classpath - either from maven plugin or by exec command
-      // try `mvn exec` for classpath
-      args = getMvnCommandArgsForMvnExec(targetPath);
-      const output = await execute('mvn', args, { cwd: targetPath });
-      classPaths = parseMvnExecCommandOutput(output);
-    } catch (e) {
-      // if it fails, try mvn dependency:build-classpath
-      // TODO send error message for further analysis
-      args = getMvnCommandArgsForDependencyPlugin(targetPath);
-      const output = await execute('mvn', args, { cwd: targetPath });
-      classPaths = parseMvnDependencyPluginCommandOutput(output);
-    }
+    await f(file.name);
   } catch (e) {
-    throw new ClassPathGenerationError(e);
+    debug(`Failed to execute command with temporary file: ${e}`);
+    throw e;
   }
-  if (classPaths.length === 0) {
-    throw new EmptyClassPathError(`mvn ${args.join(' ')}`);
+
+  try {
+    return fs.readFileSync(file.name, 'utf8');
+  } catch (e) {
+    debug(`Failed to read temporary file: ${e}`);
+    throw e;
   }
-  return mergeMvnClassPaths(classPaths);
 }
 
-export async function getClassPathFromMvn(targetPath: string): Promise<string> {
-  try {
-    const [dependenciesClassPath, outputDirectory] = await Promise.all([
-      getDependenciesClassPath(targetPath),
-      getOutputDirectory(targetPath),
+function runCommand(projectDirectory: string, args: string[]): Promise<string> {
+  return execute('mvn', args.concat(['-f', projectDirectory]), {
+    cwd: projectDirectory,
+  });
+}
+
+// Domain specific helpers
+
+async function evaluateExpression(
+  projectDirectory: string,
+  expression: string,
+): Promise<string> {
+  return await withOutputToTemporaryFile(async (outputFile) => {
+    await runCommand(projectDirectory, [
+      'help:evaluate',
+      `-Dexpression="${expression}"`,
+      `-Doutput=${outputFile}`,
     ]);
-    const fullClassPath = buildFullClassPath(
-      dependenciesClassPath,
-      outputDirectory,
-    );
-    if (fullClassPath.length === 0) {
-      throw new EmptyClassPathError('mvn');
-    }
-    return fullClassPath;
-  } catch (e) {
-    // NOTE(alexmu): Fall back to the legacy method of determining the classpath
-    debug(
-      `Failed to determine the classpath using the new method. Falling back to the legacy method. ${e}`,
-    );
-    return getClassPathFromMvnLegacy(targetPath);
+  });
+}
+
+export async function getBuildDir(baseDir: string): Promise<string> {
+  return await evaluateExpression(baseDir, 'project.build.directory');
+}
+
+export async function getOutputDir(baseDir: string): Promise<string> {
+  return await evaluateExpression(baseDir, 'project.build.outputDirectory');
+}
+
+export async function getDepsClassPath(baseDir: string): Promise<ClassPath> {
+  const classPath = await withOutputToTemporaryFile(async (outputFile) => {
+    await runCommand(baseDir, [
+      'dependency:build-classpath',
+      `-Dmdep.outputFile=${outputFile}`,
+    ]);
+  });
+  return new ClassPath(classPath);
+}
+
+export function parseModuleNames(modulesXml: string): string[] {
+  const modulesSpec = xmlJs.xml2js(modulesXml, { compact: true });
+  if ('strings' in modulesSpec && 'string' in modulesSpec['strings']) {
+    debug(`Found 'strings' in the modules XML`);
+    return modulesSpec['strings']['string'].map((s) => s['_text']);
+  } else if ('modules' in modulesSpec) {
+    debug(`Empty modules XML`);
+    return [];
+  } else {
+    throw new MalformedModulesSpecError(modulesXml);
   }
+}
+
+// Maven model
+
+export class MavenModule {
+  public readonly baseDirectory: string;
+  public readonly buildDirectory: string;
+  public readonly outputDirectory: string;
+  public readonly dependenciesClassPath: ClassPath;
+
+  constructor(
+    baseDir: string,
+    buildDirectory: string,
+    outputDirectory: string,
+    dependenciesClassPath: ClassPath,
+  ) {
+    if (buildDirectory?.length === 0) {
+      throw new Error(`Empty build directory for the project in: ${baseDir}`);
+    }
+    if (outputDirectory?.length === 0) {
+      throw new Error(`Empty output directory for the project in: ${baseDir}`);
+    }
+    if (dependenciesClassPath?.isEmpty()) {
+      throw new Error(`Empty dependencies for the project in: ${baseDir}`);
+    }
+
+    this.baseDirectory = baseDir;
+    this.buildDirectory = buildDirectory;
+    this.outputDirectory = outputDirectory;
+    this.dependenciesClassPath = dependenciesClassPath;
+  }
+
+  public getClassPath(): ClassPath {
+    debug(`Dependencies class path: ${this.dependenciesClassPath}`);
+    debug(`Output directory: ${this.outputDirectory}`);
+    return this.dependenciesClassPath.concat(
+      new ClassPath(this.outputDirectory),
+    );
+  }
+}
+
+export class MavenProject {
+  public readonly baseDir: string;
+  public readonly modules: MavenModule[];
+
+  constructor(baseDir: string, modules: MavenModule[]) {
+    if (modules?.length === 0) {
+      throw new Error(`Empty module list for the project in: ${baseDir}`);
+    }
+    this.baseDir = baseDir;
+    this.modules = modules;
+  }
+
+  public getClassPath(): string {
+    const classPaths = this.modules.map((module) => module.getClassPath());
+    const cp = classPaths.reduce((cp1, cp2) => cp1.concat(cp2));
+    debug(`Project class path: ${cp}`);
+    return cp.toString();
+  }
+}
+
+// Factories that deal with the low level details
+
+export async function makeMavenModule(baseDir: string): Promise<MavenModule> {
+  const buildDir = await getBuildDir(baseDir);
+  const outputDir = await getOutputDir(baseDir);
+  const depsClassPath = await timeIt('getMvnClassPath', async () => {
+    return await getDepsClassPath(baseDir);
+  });
+  return new MavenModule(baseDir, buildDir, outputDir, depsClassPath);
+}
+
+export async function makeMavenProject(baseDir: string): Promise<MavenProject> {
+  const modulesXml = await evaluateExpression(baseDir, 'project.modules');
+  const moduleNames = parseModuleNames(modulesXml);
+  const modules = [await makeMavenModule(baseDir)];
+  const submodules = await Promise.all(
+    moduleNames.map((name) => makeMavenModule(path.join(baseDir, name))),
+  );
+  modules.push(...submodules);
+  const validModules = modules.filter((module) =>
+    fs.existsSync(module.buildDirectory),
+  );
+  return new MavenProject(baseDir, validModules);
 }
